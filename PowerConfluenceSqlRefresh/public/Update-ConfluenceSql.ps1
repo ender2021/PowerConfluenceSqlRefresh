9df06545c2a7069e823336740924c23ad1e45943
ender2021 <meadmiracle@gmail.com>
2020-07-06 22:05:18 -0700

    added lots of error handling to the main update method

--- PowerConfluenceSqlRefresh/public/Update-ConfluenceSql.ps1
+++ PowerConfluenceSqlRefresh/public/Update-ConfluenceSql.ps1
@@ -43,6 +43,7 @@
 #>
 function Update-ConfluenceSql {
     [CmdletBinding()]
+    [OutputType([bool])]
     param (
         # Keys of specific projects to pull
         [Parameter(Position=0)]
@@ -71,6 +72,14 @@ function Update-ConfluenceSql {
         #  CONFIGURATION                                   #
         ####################################################
 
+        #set the cmdlet to stop on error, but remember the original setting so we can put it back later
+        $originalErrorAction = $ErrorActionPreference
+        $ErrorActionPreference = "Stop"
+
+        #set up some values to track occurance of errors
+        $refreshSuccess = $true
+        $refreshError = $null
+
         #configure the database targets
         $sqlSplat = @{
             SqlInstance = $SqlInstance
@@ -83,66 +92,118 @@ function Update-ConfluenceSql {
             $options = Merge-Hashtable -Source $SyncOptions -Target $options
         }
 
-        ####################################################
-        #  GET PREVIOUS BATCH INFO / CLEAR PREVIOUS BATCH  #
-        ####################################################
+        #test the sql connection
+        Write-Verbose "Testing connection to $SqlInstance"
+        if (Test-SqlConnection $SqlInstance) {
+            Write-Verbose "Connection test succeeded"
+        } else {
+            Write-Verbose "Connection test failed! Terminating refresh"
+            $ErrorActionPreference = $originalErrorAction
+            return $false
+        }
+
+        try {
+            ####################################################
+            #  GET PREVIOUS BATCH INFO / CLEAR PREVIOUS BATCH  #
+            ####################################################
+            
+            $lastRefresh = Get-LastConfluenceRefresh @sqlSplat
+            $lastRefreshStamp = $lastRefresh.Refresh_Start_Unix
+            $lastRefreshDate = $lastRefresh.Refresh_Start
+
+            ####################################################
+            #  BEGIN THE REFRESH BATCH                         #
+            ####################################################
+
+            Clear-ConfluenceStaging @sqlSplat
+            $refreshId = Start-ConfluenceRefresh @sqlSplat
+        } catch {
+            Write-Verbose "Error while initiating the batch! Terminating refresh"
+            $ErrorActionPreference = $originalErrorAction
+            Write-Error $_
+            return $false
+        }
         
-        $lastRefresh = Get-LastConfluenceRefresh @sqlSplat
-        $lastRefreshStamp = $lastRefresh.Refresh_Start_Unix
-        $lastRefreshDate = $lastRefresh.Refresh_Start
-
-        ####################################################
-        #  BEGIN THE REFRESH BATCH                         #
-        ####################################################
-
-        Clear-ConfluenceStaging @sqlSplat
-        $refreshId = Start-ConfluenceRefresh @sqlSplat
     }
     
     process {
-        ####################################################
-        #  REFRESH STEP 0 - CONFIGURE                      #
-        ####################################################
+        try {
+            ####################################################
+            #  REFRESH STEP 0 - CONFIGURE                      #
+            ####################################################
 
-        # define a convenient hash for splatting the basic refresh arguments
-        $refreshSplat = @{
-            RefreshId = $refreshId
-        } + $sqlSplat
+            # define a convenient hash for splatting the basic refresh arguments
+            $refreshSplat = @{
+                RefreshId = $refreshId
+            } + $sqlSplat
 
-        ####################################################
-        #  REFRESH STEP 1 - USERS / GROUPS                 #
-        ####################################################
+            ####################################################
+            #  REFRESH STEP 1 - USERS / GROUPS                 #
+            ####################################################
 
-        if ($options.ContainsKey("Users") -and $options.Users -ne $false) { Update-ConfluenceUsers @refreshSplat } else { Write-Verbose "Skipping Users" }
-        if ($options.ContainsKey("Groups") -and $options.Groups -ne $false) {
-            $groups = Update-ConfluenceGroups @refreshSplat | ForEach-Object { $_.Group_Name }
-            if ($options.Groups.ContainsKey("Users") -and $options.Groups.Users -ne $false) { Update-ConfluenceGroupsUsers $groups @refreshSplat } else { Write-Verbose "Skipping Groups' Users" }
-        } else { Write-Verbose "Skipping Groups" }
+            if ($options.ContainsKey("Users") -and $options.Users -ne $false) { Update-ConfluenceUsers @refreshSplat } else { Write-Verbose "Skipping Users" }
+            if ($options.ContainsKey("Groups") -and $options.Groups -ne $false) {
+                $groups = Update-ConfluenceGroups @refreshSplat | ForEach-Object { $_.Group_Name }
+                if ($options.Groups.ContainsKey("Users") -and $options.Groups.Users -ne $false) { Update-ConfluenceGroupsUsers $groups @refreshSplat } else { Write-Verbose "Skipping Groups' Users" }
+            } else { Write-Verbose "Skipping Groups" }
 
-        ####################################################
-        #  REFRESH STEP 2 - SPACES                         #
-        ####################################################
+            ####################################################
+            #  REFRESH STEP 2 - SPACES                         #
+            ####################################################
 
-        if ($options.ContainsKey("Spaces") -and $options.Spaces -ne $false) {
-            $settings = IIF ($options.Spaces.GetType().Name -eq "Hashtable") $options.Spaces $null
-            Update-ConfluenceSpaces $SpaceKeys $settings @refreshSplat
-        } else { Write-Verbose "Skipping Users" }
+            if ($options.ContainsKey("Spaces") -and $options.Spaces -ne $false) {
+                $settings = IIF ($options.Spaces.GetType().Name -eq "Hashtable") $options.Spaces $null
+                Update-ConfluenceSpaces $SpaceKeys $settings @refreshSplat
+            } else { Write-Verbose "Skipping Users" }
 
-        ####################################################
-        #  REFRESH STEP X - SYNC STAGING TO LIVE TABLES    #
-        ####################################################
+            ####################################################
+            #  REFRESH STEP X - SYNC STAGING TO LIVE TABLES    #
+            ####################################################
 
-        #perform the sync
-        Sync-ConfluenceStaging @sqlSplat
+            #perform the sync
+            Sync-ConfluenceStaging @sqlSplat
+        } catch {
+            ####################################################
+            #  HANDLE ERROR OCCURING DURING REFRESH            #
+            ####################################################
+            Write-Verbose "Error while executing the batch! Terminating refresh"
+            $refreshSuccess = $false
+            $refreshError = $_
+        }
     }
     
     end {
         ####################################################
         #  RECORD BATCH END                                #
         ####################################################
+        $batchTermError = $null
+        try {
+            Stop-ConfluenceRefresh @refreshSplat -Success $refreshSuccess
+        } catch {
+            Write-Verbose "Error while recording batch end! Exiting without recording"
+            $refreshSuccess = $false
+            $batchTermError = $_
+        }
 
-        Stop-ConfluenceRefresh @refreshSplat
+        ####################################################
+        #  RETURN SUCCESS / FAILURE, OUTPUT ERRORS         #
+        ####################################################
+        $ErrorActionPreference = $originalErrorAction
+        if ($refreshSuccess) {
+            Write-Verbose "Confluence update completed successfully!"
+            return $true
+        } else {
+            Write-Verbose "Confluence updated completed with errors :("
+            if ($refreshError) {
+                Write-Verbose "Terminating Error:"
+                Write-Error $refreshError
+            }
+            if ($batchTermError) {
+                Write-Verbose "Batch End Recording Error:"
+                Write-Error $batchTermError
+            }
 
-        Write-Verbose "Confluence update completed!"
+            return $false
+        }
     }
 }
