985a18c84d6e47ff67c06d9eece37de7a380edb8
ender2021 <meadmiracle@gmail.com>
2020-07-06 21:48:25 -0700

    add sql connection test utility stolen from stack overflow

---
+++ PowerConfluenceSqlRefresh/private/utility/Test-SqlConnection.ps1
@@ -0,0 +1,24 @@
+#Taken from: https://stackoverflow.com/questions/29229109/test-database-connectivity
+function Test-SqlConnection
+{    
+    [OutputType([bool])]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                    ValueFromPipelineByPropertyName=$true,
+                    Position=0)]
+        $ConnectionString
+    )
+    try
+    {
+        $sqlConnection = New-Object System.Data.SqlClient.SqlConnection $ConnectionString;
+        $sqlConnection.Open();
+        $sqlConnection.Close();
+
+        return $true;
+    }
+    catch
+    {
+        return $false;
+    }
+}
